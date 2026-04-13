a3ee65268ed13c417ab5865c833d14c5d694d131
Anderson-Leandro <106823753+Anderson-Leandro@users.noreply.github.com>
2026-04-13 23:58:02 +0000

    Adicionados modelo e rotas de contato, corrigidos imports

---
+++ app.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+app.use(express.json());
+
+const contatoRouter = require('./routes/contatoRoutes');
+app.use('/contatos', contatoRouter);
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erro de conexão ao MongoDB:'));
+db.once('open', () => {
+  console.log('Conectado ao MongoDB Atlas!');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
